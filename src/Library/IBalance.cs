04f6477ca06185ce8bbfe44954a4420fe4adb486
Felipe Villaronga <felipe.villaronga@correo.ucu.edu.uy>
2022-09-26 08:36:59 -0300

    nuevas interfaces

---
+++ src/Library/IBalance.cs
@@ -0,0 +1,4 @@
+/*
+bool Transaction(bool operation, int value); //hace una transaccion ,el operation determina si se añade o pierde dinero, el bool determina si fue posible o no
+    int GetCoins();  //get de coins
+    */
---
+++ src/Library/IInventory.cs
@@ -0,0 +1,4 @@
+/*void InventoryAdd(IItems item);
+    void InventoryRemove(IItems item);
+    public void Equip(IItems item); //equipamiento de items --> afecta al inventario
+    public void Desequip(IItems item); //desequipamiento de items --> afecta al inventario*/
